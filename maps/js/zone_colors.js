2c7cf5994d7d20f0eefa793dd0d1c4d7d9f03142
vcunningham <vcunningham@wisc.edu>
2020-04-21 15:00:26 -0500

    Added home button to maps
    Quick home button to allow for fluid site navigation

--- maps/js/zone_colors.js
+++ maps/js/zone_colors.js
@@ -37,7 +37,26 @@ function setMap(zones) {
 	createOpacityControls()
 	createLegend()
 	
+	createHomeButton();
 };
+
+// quick home button to get back to the home page
+function createHomeButton(){
+	var home = L.Control.extend({
+		options: {
+            position: 'bottomleft'
+        },
+		onAdd: function(){
+			var homeButton = L.DomUtil.create('div', 'homeDiv');
+			
+			$(homeButton).append('<button onclick="redirect(\'..\')">Home</button>');
+			
+			return homeButton;
+		}
+	});
+	map.addControl(new home());
+}
+
 function createProposals(){
 	var rowBar = L.Control.extend({
         options: {
