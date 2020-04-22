8366c7efafdc9942390c69d4de73dfc1b31fcf5e
vcunningham <vcunningham@wisc.edu>
2020-04-22 17:25:28 -0500

    Initial Popup implemented
    When first loading in the popup displays, can be closed, and has a next button that loops through the two page contents I copied over. Will wait to add to other pages until full styling and whatnot

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 
+var overlayTrigger = false;
+
 // basic relative redirect until we have a nav-menu
 // just subfolder name should be passed to this one
+
 function redirect(path){
 	window.location.href= path;
+}
+
+function overlayOff(){
+	document.getElementById("overlay").style.display = "none";
+}
+
+function overlayOn(){
+	document.getElementById("overlay").style.display = "block";
+}
+
+function overlayShowNext(){
+	if(!overlayTrigger){
+		document.getElementById("assign-overlay-text").style.display = "none";
+		document.getElementById("tambopata-overlay-text").style.display = "block";
+		overlayTrigger = true;
+	} else {
+		document.getElementById("assign-overlay-text").style.display = "block";
+		document.getElementById("tambopata-overlay-text").style.display = "none";
+		overlayTrigger = false;
+	}
 }
