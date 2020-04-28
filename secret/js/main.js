c491537ddb41aa5421a42992471a5799332c4673
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-04-27 19:28:45 -0500

    Mobile Nav Bar in credits page

--- secret/js/main.js
+++ secret/js/main.js
@@ -4,7 +4,21 @@
 function redirect(path){
 	window.location.href= path;
 }
+function mobile(){
+	$('.navbar1').remove();
+	
+	var bottomNavBar = $("<div id = 'navbar2'></div>")
+	bottomNavBar.appendTo($("body"));
+	$(bottomNavBar).append('<button id = "bottomNav" class="col-sm-2.4 col-xs-2.4" onclick="overlayOn()"><img src="img/NavbarImg/Assignment.png" width="30" height="30" class="d-inline-block align-top" alt=""></button>');
+	$(bottomNavBar).append('<button id = "bottomNav" class="active col-sm-2.4 col-xs-2.4"><img src="img/NavbarImg/person.png" width="30" height="30" class="d-inline-block align-top"alt=""></button>');
+	$(bottomNavBar).append('<button id = "bottomNav" class="col-sm-2.4 col-xs-2.4" onclick="redirect(\'maps\')"><img src="img/NavbarImg/map.png" width="30" height="30" class="d-inline-block align-top"alt=""></button>');
+	$(bottomNavBar).append('<button id = "bottomNav" class="col-sm-2.4 col-xs-2.4" onclick="redirect(\'secret\')"><img src="img/NavbarImg/lock.png" width="30" height="30" class="d-inline-block align-top"alt=""></button>');
+	$(bottomNavBar).append('<button id = "bottomNav" class="col-sm-2.4 col-xs-2.4" onclick="redirect(\'credits\')"><img src="img/NavbarImg/info.png" width="30" height="30" class="d-inline-block align-top"alt=""></button>');
+};
 
+if(window.innerWidth < 780){
+	mobile()
+}
 
 // Open source slider modified and copied under MIT License
 // Call & init
