219fe7db08a750e50a69efb25f072bc6945f78df
vcunningham <vcunningham@wisc.edu>
2020-04-28 18:49:44 -0500

    Various Changes
    Added glossary and events to about popup, added a popup nav-bar, fixed code for sessionStorage, made the page not scrollable when popup is opened, and added a resized image for the oil officer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-	
-var overlayTrigger = false;
 
 // basic relative redirect until we have a nav-menu
 // just subfolder name should be passed to this one
@@ -8,34 +6,54 @@ function redirect(path){
 	window.location.href= path;
 }
 
+// checks the local session storage to see if we've added a var
+//     if we have: don't show popup, if we haven't: set the var
 function checkVisited(){
-	var ls = sessionStorage.getItem('namespace.visited');
+	var ls = sessionStorage.getItem('tambopata.visited');
 	if (ls != null) {
 		overlayOff();
 	}else{
-		sessionStorage.setItem('namespace.visited', 1)
+		sessionStorage.setItem('tambopata.visited', "visited")
 	}
 }
 
+// if they hit the close button then hide the overlay
 function overlayOff(){
+	document.documentElement.style.overflow = "auto";
 	document.getElementById("overlay").style.display = "none";
 }
-
+// same but show on assign click
 function overlayOn(){
+	document.documentElement.style.overflow = "hidden";
 	document.getElementById("overlay").style.display = "block";
 }
 
-function overlayShowNext(){
-	if(!overlayTrigger){
-		document.getElementById("assign-overlay-text").style.display = "none";
-		document.getElementById("tambopata-overlay-text").style.display = "block";
-		overlayTrigger = true;
-	} else {
+// basic switching of overlay content on button press
+function overlayShowNext(next){
+	if(next == "assign"){
 		document.getElementById("assign-overlay-text").style.display = "block";
 		document.getElementById("tambopata-overlay-text").style.display = "none";
-		overlayTrigger = false;
+		document.getElementById("glossary-overlay-text").style.display = "none";
+		document.getElementById("events-overlay-text").style.display = "none";
+	} else if(next == "tambopata"){
+		document.getElementById("assign-overlay-text").style.display = "none";
+		document.getElementById("tambopata-overlay-text").style.display = "block";
+		document.getElementById("glossary-overlay-text").style.display = "none";
+		document.getElementById("events-overlay-text").style.display = "none";
+	}
+	else if(next == "glossary"){
+		document.getElementById("assign-overlay-text").style.display = "none";
+		document.getElementById("tambopata-overlay-text").style.display = "none";
+		document.getElementById("glossary-overlay-text").style.display = "block";
+		document.getElementById("events-overlay-text").style.display = "none";
+	} else {
+		document.getElementById("assign-overlay-text").style.display = "none";
+		document.getElementById("tambopata-overlay-text").style.display = "none";
+		document.getElementById("glossary-overlay-text").style.display = "none";
+		document.getElementById("events-overlay-text").style.display = "block";
 	}
 }
 
-//window.onload(checkVisited());
+// This calls checkVisited on each load
+window.onload = checkVisited();
 
