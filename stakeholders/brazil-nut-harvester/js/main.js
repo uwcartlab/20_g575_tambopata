a473a67224842ef8f0bbdc51687921ef0eca43ac
ajkuehl <60198331+ajkuehl@users.noreply.github.com>
2020-04-30 22:00:27 -0500

    Brazil Nut Stakeholder, reference articles added, responsive sticky scroll to top button added

--- stakeholders/brazil-nut-harvester/js/main.js
+++ stakeholders/brazil-nut-harvester/js/main.js
@@ -130,4 +130,24 @@ function popupREDD() {
   var popup6 = document.getElementById("myPopup6");
     popup6.classList.toggle("show");
 };
-// </script>
+
+// Jump to top of page, referenced from w3schools.com
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
