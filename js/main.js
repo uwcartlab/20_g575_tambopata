3ee2c5ce7097991d925fc1d21c9818f6fdde2014
vcunningham <vcunningham@wisc.edu>
2020-05-02 15:28:51 -0500

    Changes to About Popup
    Quick styling changes and a few other things

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // basic relative redirect until we have a nav-menu
 // just subfolder name should be passed to this one
 var pass;
+var prevButton = "tambopata-button";
 
 function redirect(path){
 	if(path != 'secret'){
@@ -17,7 +18,6 @@ function checkVisited(){
 	fetch('img/pass.txt')
 	.then(response => response.text())
 	.then((data) => {
-	  console.log(data);
 	  pass = data;
 	});
 	
@@ -42,11 +42,13 @@ function secretAccess(path){
 function overlayOff(){
 	document.documentElement.style.overflow = "auto";
 	document.getElementById("overlay").style.display = "none";
+	document.getElementById("overlay-img").style.display = "none";
 }
 // same but show on assign click
 function overlayOn(){
 	document.documentElement.style.overflow = "hidden";
 	document.getElementById("overlay").style.display = "block";
+	document.getElementById("overlay-img").style.display = "block";
 }
 
 // basic switching of overlay content on button press
@@ -56,22 +58,48 @@ function overlayShowNext(next){
 		document.getElementById("tambopata-overlay-text").style.display = "none";
 		document.getElementById("glossary-overlay-text").style.display = "none";
 		document.getElementById("events-overlay-text").style.display = "none";
+		
+		document.getElementById(prevButton).style.backgroundColor = "#00a642";
+		document.getElementById(prevButton).style.color = "white";
+		document.getElementById("assign-button").style.backgroundColor = "#05f765";
+		document.getElementById("assign-button").style.color = "black";
+		prevButton = "assign-button";
 	} else if(next == "tambopata"){
 		document.getElementById("assign-overlay-text").style.display = "none";
 		document.getElementById("tambopata-overlay-text").style.display = "block";
 		document.getElementById("glossary-overlay-text").style.display = "none";
 		document.getElementById("events-overlay-text").style.display = "none";
+		
+		document.getElementById(prevButton).style.backgroundColor = "#00a642";
+		document.getElementById(prevButton).style.color = "white";
+		document.getElementById("tambopata-button").style.backgroundColor = "#05f765";
+		document.getElementById("tambopata-button").style.color = "black";
+		prevButton = "tambopata-button";
 	}
 	else if(next == "glossary"){
 		document.getElementById("assign-overlay-text").style.display = "none";
 		document.getElementById("tambopata-overlay-text").style.display = "none";
 		document.getElementById("glossary-overlay-text").style.display = "block";
 		document.getElementById("events-overlay-text").style.display = "none";
+		
+		document.getElementById(prevButton).style.backgroundColor = "#00a642";
+		document.getElementById(prevButton).style.color = "white";
+		document.getElementById("glossary-button").style.backgroundColor = "#05f765";
+		document.getElementById("glossary-button").style.color = "black";
+		prevButton = "glossary-button";
+		
 	} else {
 		document.getElementById("assign-overlay-text").style.display = "none";
 		document.getElementById("tambopata-overlay-text").style.display = "none";
 		document.getElementById("glossary-overlay-text").style.display = "none";
 		document.getElementById("events-overlay-text").style.display = "block";
+		
+		document.getElementById(prevButton).style.backgroundColor = "#00a642";
+		document.getElementById(prevButton).style.color = "white";
+		document.getElementById("events-button").style.backgroundColor = "#05f765";
+		document.getElementById("events-button").style.color = "black";
+		prevButton = "events-button";
+		
 	}
 }
 
