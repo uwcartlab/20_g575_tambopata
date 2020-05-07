731e5a1f63d23192d3306af7eb9cda7def49ebba
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-05-06 21:09:08 -0500

    Update Map Color Scheme

--- maps/js/mobile.js
+++ maps/js/mobile.js
@@ -381,7 +381,7 @@ function style(feature){
 	var color; // color of the zone
     var zoneName = feature.properties.ZONES
 	if(zoneName == "Buffer Zone"){ // if it's the buffer zone, make it Powder blue
-	color = "#b29d77";
+	color = "#4d412b";
 	lineWidth = 0.3;
 	lineColor = "Black";
 	fillop = opacity
@@ -436,7 +436,7 @@ function style(feature){
 			fillop = opacity;
 		}
 		else if(zoneName == "Community Reserve"){
-			color = "#f5aa1c";
+			color = "#a87007";
 			lineWidth = 0.1;
 			lineColor = "Black";
 			fillop = opacity;
